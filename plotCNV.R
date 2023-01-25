46fb6b2502c488d7a574435030533ec36ddd52b3
Brady Neeley <mbradyneeley@gmail.com>
2023-01-25 21:26:00 +0000

    Added functionality to create addtl plot with gene region bars, input testing needed

--- plotCNV.R
+++ plotCNV.R
@@ -1,9 +1,24 @@
 #!/usr/bin/Rscript
 
+suppressPackageStartupMessages(library(biomaRt))
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(ggplot2))
 
+# Reads in cramFileName, startPosCNV, endPosCNV, geneName
 args = commandArgs(TRUE)
+
+# Read in gene info
+gene = c(args[4])
+# Annotate with position
+mart <- useMart("ensembl")
+mart <- useDataset("hsapiens_gene_ensembl", mart)
+gene_positions <- getBM(
+  attributes = c('hgnc_symbol', 'chromosome_name', 'start_position', 'end_position'),
+  filters = 'hgnc_symbol',
+  values = gene,
+  mart = mart
+)
+rm(mart)
 
 # Testing code
 df = read_tsv("normalized.bin.count.txt",
@@ -18,7 +33,20 @@ fin = separate(data = good, col = `c(test$V1, test$V2)`, into = c("idx", "pos", 
 fin$pos = as.numeric(fin$pos)
 fin$val = as.numeric(fin$val)
 
-ggplot(data = fin, aes(x=pos, y=val)) + geom_line() + xlab("Position") + ylab("Log Count Aligned Overlaps")
+ggplot(data = fin, aes(x=pos, y=val)) +
+	geom_line() +
+	geom_vline(xintercept = c(gene_positions$start_position, gene_positions$end_position), color = "blue") +
+	xlab("Position") +
+	ylab("Log Count Aligned Overlaps")
+
+#save plot
+ggsave(paste0(args[1],"_", args[2], "_", args[3], "_CNV_plot_with_Gene_start_end.png"), units = "cm", width = 20, height = 10)
+# File format cramfileName_startCNV_endCNV_CNV_plot.png
+
+ggplot(data = fin, aes(x=pos, y=val)) +
+	geom_line() +
+	xlab("Position") +
+	ylab("Log Count Aligned Overlaps")
 
 #save plot
 ggsave(paste0(args[1],"_", args[2], "_", args[3], "_CNV_plot.png"), units = "cm", width = 20, height = 10)
--- vizualizeAlignmentsWorkflow.sh
+++ vizualizeAlignmentsWorkflow.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
-# Change the start and end positions of the CNV you want to visualize here:
-start_region=155218456
-end_region=155241513
+# Pass in as cmd args your start, end, and chr#
+start_region=$3
+end_region=$4
 # Set the flanking region that will be visualized on either side
 # of the CNV window specified above:
 flank=200000
 # Set the chromosome number
-chr=chr1
+chr=$2
+# Pass in gene name
+gene=$5
 
 # provide cram as cmd line argument
 cram=$1
 
 if [ -z $1 ]; then
 	echo "Please include cram/bam file as additional argument"
+	exit 0
+fi
+if [ -z $2 ]; then
+	echo "Please include all command args as ./script <cram> <chr#> <start> <end> <geneName>"
+	exit 0
+fi
+if [ -z $3 ]; then
+	echo "Please include all command args as ./script <cram> <chr#> <start> <end> <geneName>"
+	exit 0
+fi
+if [ -z $4 ]; then
+	echo "Please include all command args as ./script <cram> <chr#> <start> <end> <geneName>"
+	exit 0
+fi
+if [ -z $5 ]; then
+	echo "Please include all command args as ./script <cram> <chr#> <start> <end> <geneName>"
 	exit 0
 fi
 
@@ -44,7 +62,7 @@ awk -v mean=`awk '{ total += $5 } END { print total/NR }' ${fileName}.multicov.t
 	> normalized.bin.count.txt
 
 # Produce plot
-Rscript --vanilla plotCNV.R $fileName $start_region $end_region
+Rscript --vanilla plotCNV.R $fileName $start_region $end_region $gene
 
 # Clean up
 rm normalized.bin.count.txt *.multicov.tsv intervals.txt
